553fc71d244f6cd7ef658ad8a117ffeea78084c8
Lucy <lucydenisenko@gmail.com>
2022-05-23 21:43:39 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a number of dogs you want to feed: ");
+
+        int numberOfDogs = Integer.parseInt(reader.readLine());
+        System.out.println("You have: " + numberOfDogs + " dogs");
+
+        System.out.println("Enter a number of cats you want to feed: ");
+
+        int numberOfCats = Integer.parseInt(reader.readLine());
+        System.out.println("You have: " + numberOfCats + " cats");
+
+        System.out.println("Enter a number of days: ");
+
+        int numberOfDays = Integer.parseInt(reader.readLine());
+        System.out.println("Number of days: " + numberOfDays);
+
+
+        Shelter shelter = new Shelter(numberOfCats,numberOfDogs, 2, numberOfDays);
+        System.out.println("----------------------------");
+
+        System.out.println("For " + numberOfDogs + " (dog)s you need " + shelter.eatPerDogs() + " g dog food "
+                + " for " + numberOfDays + " day(s)");
+
+        System.out.println("For " + numberOfCats + " cat(s) you need " + shelter.eatPerCats() + " g cat food "
+                + " for " + numberOfDays + " day(s)");
+
+        System.out.println("Overall cost of the shelter maintenance is " + "$" + shelter.overalCost());
+    }
+}
---
+++ src/Shelter.java
@@ -0,0 +1,25 @@
+public class Shelter {
+    private int cats;
+    private int dogs;
+    private int costForItem;
+    private int days;
+
+    public Shelter(int cats, int dogs, int costForItem, int days) {
+        this.cats = cats;
+        this.dogs = dogs;
+        this.costForItem = costForItem;
+        this.days = days;
+    }
+
+    public int eatPerCats() {
+        return this.cats * 50 * 3 * this.days;
+    }
+
+    public int eatPerDogs() {
+        return this.dogs * 50 * 3 * this.days;
+    }
+
+    public int overalCost() {
+        return (3 * this.days * (this.cats + this.dogs)) * costForItem;
+    }
+}
